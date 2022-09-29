80128d93ac8588ad4aea0fbea90016484a3fa4f6
Denis Fedorov <ssboshen@yandex.ru>
2022-09-29 15:40:16 +0300

    Simple Leetcode task

--- src/weakestRowInArray/weakestRowBySoldier.java
+++ src/weakestRowInArray/weakestRowBySoldier.java
@@ -1,6 +1,7 @@
 package weakestRowInArray;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class weakestRowBySoldier {
     public static void main(String[] args) {
@@ -12,20 +13,35 @@ public class weakestRowBySoldier {
                 {1, 1, 1, 1, 1}
         };
         int[] result = kWeakestRows(mat, 3);
+        System.out.println(Arrays.toString(result));
     }
 
     public static int[] kWeakestRows(int[][] mat, int k) {
+
+        Map<Integer, List<Integer>> resultMap = new TreeMap<>();
+
         int sum = 0;
-        int[] result = new int[mat.length];
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
                 sum += mat[i][j];
             }
-            result[i] = sum;
+            if (!resultMap.containsKey(sum)) {
+                resultMap.put(sum, new ArrayList<>());
+            }
+            resultMap.get(sum).add(i);
             sum = 0;
         }
-        for (int i = 0; i < result.length; i++) {
-            System.out.printf("Номер %d, скилько солдат %d\n", i, result[i]);
+
+        int pos = 0;
+        int[] result = new int[k];
+        for (int key : resultMap.keySet()) {
+            if (pos == k) break;
+            List<Integer> list = resultMap.get(key);
+            for (int value : list) {
+                if (pos == k) break;
+                result[pos] = value;
+                pos++;
+            }
         }
         return result;
     }
